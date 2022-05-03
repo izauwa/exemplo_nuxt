3bc90ac19fb1f24517853fab5a6fd7b6656bd433
unknown <izabelle.santos@aluno.ifsp.edu.br>
2022-05-03 16:40:51 -0300

    Adiçao do projeto Nuxt.js

--- newproject/pages/index.vue
+++ newproject/pages/index.vue
@@ -1,9 +1,51 @@
 <template>
-  <Tutorial/>
+  <div class="container">
+    <h1>{{titulo}}</h1>
+    <ul>
+      <li :key="item.id" v-for = "item in pessoas">
+      </li>
+    </ul>
+    <button @click="digaOla">Diga olá!</button>
+    </div>
+
 </template>
 
 <script>
 export default {
-  name: 'IndexPage'
-}
+  data: function(){
+    return{
+      titulo:"Lista de pessoas e idade",
+      pessoas: [
+        {id: 1, nome: "Marcio", idade:"33"},
+        {id: 2, nome: "Diego", idade:"44"},
+        {id: 3, nome: "Launa", idade:"50"},
+        {id: 4, nome: "Tamara", idade:"23"},
+
+    ]
+
+    }
+  } ,
+  methods:{
+    digaOla: function(){
+
+      alert("Olá Mundo do Nuxt.js!");
+
+    }
+
+
+    }
+
+
+  }
+ 
+
 </script>
+
+<style scoped>
+
+.container{
+
+  margin: 20px
+
+}
+</style>
